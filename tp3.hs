b09c2671bba923a49a1942e77cbeb04bf42c8684
LorenzoFerraces <lorenzoferraces@proton.me>
2023-03-30 14:50:32 -0300

    avance tp 3

--- tp3.hs
+++ tp3.hs
@@ -1,4 +1,3 @@
---Pruebas
 
 doble :: Int -> Int
 doble x = x + x
@@ -15,7 +14,7 @@ uncurry f (x,y) = f x y
 
 -- a. 
 apply :: (a -> b) -> a -> b
-apply f x = f x
+apply f = f 
 
 -- b.
 twice :: (a -> a) -> a -> a
@@ -43,3 +42,61 @@ const x y = x
 -- g.
 compose :: (a -> b) -> (c -> a) -> c -> b
 compose f g x = f (g x)
+
+--4)
+{-
+
+--a.
+apply apply apply :: (a -> b) -> a -> b
+
+
+--b.
+(twice doble) 2 :: Int
+
+
+--c.
+twice twice twice swap :: (a, b) -> (a, b)
+
+--d.
+flip twice 1 doble :: Int
+-}
+
+
+--5)
+
+--a. 
+{-
+appDup f = g 
+    where g x = f (x, x)
+-}
+
+appDup :: ((a,a) -> b) -> a -> b 
+appDup = \f -> \x -> f(x, x)
+
+--b.
+{-
+appFork (f, g) = h 
+    where h x = (f x, g x)
+-}
+appFork :: (a -> b, a -> c) -> a -> (b, c)
+appFork = \(f,g) -> \x -> (f x, g x)
+--c.
+{-
+appPar (f, g) = h 
+    where h (x, y) = (f x, g y)
+-}
+appPar :: (a -> b, c -> d) -> (a,c) -> (b, d)
+appPar = \(f,g) -> \(x,y) -> (f x, g y)
+
+--d.
+{-
+appDist f = g
+    where g (x, y) = (f x, g y)
+-}
+
+--e.
+{-
+subst f = h
+    where h g = k
+        where k x = (f x) (g x)
+-}
