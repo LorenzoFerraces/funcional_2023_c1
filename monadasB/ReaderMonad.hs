3c008212dc9518180c29a4c652886b5b8c34a0dd
LorenzoFerraces <lorenzoferraces@proton.me>
2023-07-07 20:10:04 -0300

    commit prueba action

--- monadasB/ReaderMonad.hs
+++ monadasB/ReaderMonad.hs
@@ -6,6 +6,7 @@ import Monadasb
 
 data Reader r a = R (r -> a)
 
+-- cambiar a version vieja de ghc para hacer funcionar
 instance Monad (Reader r) where
   return x = R (\r -> x)
   m >>= k = R (\r -> let R fm = m
@@ -16,3 +17,4 @@ instance ReaderMonad r (Reader r) where
   ask = R (\r -> r)
   runRM m r = let R fm = m
                 in fm r
+
